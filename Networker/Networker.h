e628f3e2301e186bf668c1325062c7089c8992ef
Jon Bash <jonbash@protonmail.com>
2020-01-24 16:45:22 -0800

    Initial Commit

---
+++ Networker/Networker.h
@@ -0,0 +1,19 @@
+//
+//  Networker.h
+//  Networker
+//
+//  Created by Jon Bash on 2020-01-24.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Networker.
+FOUNDATION_EXPORT double NetworkerVersionNumber;
+
+//! Project version string for Networker.
+FOUNDATION_EXPORT const unsigned char NetworkerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networker/PublicHeader.h>
+
+
