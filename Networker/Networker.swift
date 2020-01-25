dc4fe41a67d49bcde58bb09fbfd78e10d215a8ce
Jon Bash <jonbash@protonmail.com>
2020-01-25 13:29:29 -0800

    refactor networker methods

--- Networker/Networker.swift
+++ Networker/Networker.swift
@@ -14,7 +14,6 @@ class Networker {
 
     var dataLoader: DataLoader
     var decoder: DataDecoder
-
     var verboseLogging: Bool
 
     // MARK: - Init
@@ -30,101 +29,48 @@ class Networker {
 
     // MARK: - Fetch
 
-    func fetch<T: Decodable>(
-        _ type: T.Type,
-        fromURL url: URL,
-        completion: @escaping (Result<T, NetworkError>) -> Void
-    ) -> URLSessionDataTask {
-        return fetch(T.self, withRequest: URLRequest(url: url), completion: completion)
-    }
+    // TODO: refactor so there's not so many nested calls
 
-    func fetch<T: Decodable>(
-        _ type: T.Type,
+    func fetch<DecodableModel: Decodable>(
+        _ type: DecodableModel.Type,
         withRequest request: URLRequest,
-        completion: @escaping (Result<T, NetworkError>) -> Void
+        completion: @escaping (Result<DecodableModel, NetworkError>) -> Void
     ) -> URLSessionDataTask {
-        return fetchOptional(T.self, withRequest: request) { result in
-            switch result {
-            case .success(let possibleModel):
-                if let model = possibleModel {
-                    completion(.success(model))
-                } else {
-                    completion(.failure(.noData))
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
+        return fetchData(withRequest: request) { [weak self] result in
+            guard let self = self else { return }
+            self.handleDataResult(result, completion: completion)
         }
     }
 
-    func fetchOptional<T: Decodable>(
-        _ type: T.Type,
-        fromURL url: URL,
-        completion: @escaping (Result<T?, NetworkError>) -> Void
+    func fetchOptional<DecodableModel: Decodable>(
+        _ type: DecodableModel.Type,
+        withRequest request: URLRequest,
+        completion: @escaping (Result<DecodableModel?, NetworkError>) -> Void
     ) -> URLSessionDataTask {
-        return fetchOptional(T.self, withRequest: URLRequest(url: url), completion: completion)
+        return fetchOptionalData(withRequest: request) { [weak self] result in
+            guard let self = self else { return }
+            self.handleOptionalDataResult(result, completion: completion)
+        }
     }
 
-    func fetchOptional<T: Decodable>(
-        _ type: T.Type,
+    func fetchData(
         withRequest request: URLRequest,
-        completion: @escaping (Result<T?, NetworkError>) -> Void
+        completion: @escaping (Result<Data, NetworkError>) -> Void
     ) -> URLSessionDataTask {
         return fetchOptionalData(withRequest: request) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let data):
-                guard let data = data else {
-                    completion(.success(nil))
-                    return
-                }
-                do {
-                    let model = try self.decoder.decode(T.self, from: data)
-                    completion(.success(model))
-                } catch {
-                    completion(.failure(.dataCodingError(specifically: error, data: data)))
-                }
-            case .failure(let error):
-                if error == .noData {
-                    completion(.success(nil))
-                } else {
-                    completion(.failure(error))
-                }
-            }
-        }
-    }
-
-    func fetchData(
-        fromURL url: URL,
-        completion: @escaping (Result<Data, NetworkError>) -> Void
-    ) -> URLSessionDataTask {
-        return fetchData(withRequest: URLRequest(url: url), completion: completion)
-    }
-
-    func fetchData(
-        withRequest request: URLRequest,
-        completion: @escaping (Result<Data, NetworkError>) -> Void
-    ) -> URLSessionDataTask {
-        return fetchOptionalData(withRequest: request) { result in
-            switch result {
-            case .success(let data):
                 if let data = data {
                     completion(.success(data))
                 } else {
+                    self.log("No data!")
                     completion(.failure(.noData))
                 }
             case .failure(let error):
                 completion(.failure(error))
             }
         }
-    }
-
-    func fetchOptionalData(
-        fromURL url: URL,
-        completion: @escaping (Result<Data?, NetworkError>) -> Void
-    ) -> URLSessionDataTask {
-        return fetchOptionalData(withRequest: URLRequest(url: url),
-                                 completion: completion)
     }
 
     func fetchOptionalData(
@@ -133,13 +79,11 @@ class Networker {
     ) -> URLSessionDataTask {
         return dataLoader.dataTask(with: request) { [weak self] data, response, error in
             guard let self = self else { return }
-
             if let error = error {
                 self.log("Error with request \(request): \(error)")
                 completion(.failure(.other(specifically: error)))
                 return
             }
-
             guard let httpResponse = response as? HTTPURLResponse,
                 httpResponse.statusCode >= 200 && httpResponse.statusCode < 300
                 else {
@@ -147,16 +91,58 @@ class Networker {
                     completion(.failure(.non200Response(response: response, data: data)))
                     return
             }
-
+            self.log("Passing on (possible) fetched data: \(String(describing: data))")
             completion(.success(data))
         }
     }
 
     // MARK: - Private
 
-    private func log(_ string: String) {
+    private func handleDataResult<DecodableModel: Decodable>(
+        _ result: Result<Data, NetworkError>,
+        completion: @escaping (Result<DecodableModel, NetworkError>) -> Void
+    ) {
+        switch result {
+        case .success(let data):
+            do {
+                let model = try self.decoder.decode(DecodableModel.self, from: data)
+                completion(.success(model))
+            } catch {
+                completion(.failure(.dataCodingError(specifically: error, data: data)))
+            }
+        case .failure(let error):
+            completion(.failure(error))
+        }
+    }
+
+    private func handleOptionalDataResult<DecodableModel: Decodable>(
+        _ result: Result<Data?, NetworkError>,
+        completion: @escaping (Result<DecodableModel?, NetworkError>) -> Void
+    ) {
+        switch result {
+        case .success(let data):
+            guard let data = data else {
+                completion(.success(nil))
+                return
+            }
+            do {
+                let model = try self.decoder.decode(DecodableModel.self, from: data)
+                completion(.success(model))
+            } catch {
+                completion(.failure(.dataCodingError(specifically: error, data: data)))
+            }
+        case .failure(let error):
+            if error == .noData {
+                completion(.success(nil))
+            } else {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    private func log<S: ExpressibleByStringLiteral>(_ string: S) {
         if verboseLogging {
-            print(string)
+            print("Networker: \(string)")
         }
     }
 }
